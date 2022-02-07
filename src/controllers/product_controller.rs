90b476c61108be3beeda1be1fc7944446b232b4f
molnarattila991 <molnarattila991@gmail.com>
2022-02-07 19:13:15 +0100

    (edit) create controller mod

---
+++ src/controllers/product_controller.rs
@@ -0,0 +1,42 @@
+use mysql::*;
+use mysql::prelude::*;
+use actix_web::{get, web, HttpResponse, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Product {
+    id: u64,
+    code: String,
+    price: f32,
+    product_name: String,
+}
+
+#[get("/")]
+pub async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/jsonList")]
+pub async fn hello2() -> impl Responder {
+    // let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
+    // let url = "mysql://shop-api:shop-api@172.21.192.1:3306/shop-api";
+    // let url = "mysql://shop-api:shop-api@db:3306/shop-api";
+    let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
+
+    let opt = Opts::from_url(url);
+    let pool = Pool::new(opt.unwrap()).unwrap();
+    let mut _conn = pool.get_conn().unwrap();
+
+    let res = _conn.query_map(
+        "select product_id, product_code, price, name from PRODUCT",
+        |(product_id, product_code, price, name)| 
+          Product {
+                id: product_id,
+                code: product_code,
+                price: price,
+                product_name: name
+            }
+    ).expect("Query failed.");
+     
+    web::Json(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,7 @@
-use mysql::*;
-use mysql::prelude::*;
-// use chrono::prelude::*; //For date and time
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use serde::Serialize;
+mod controllers;
 
-#[derive(Serialize)]
-struct Product {
-    id: u64,
-    code: String,
-    price: f32,
-    product_name: String,
-}
-
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-
-#[get("/jsonList")]
-async fn hello2() -> impl Responder {
-    // let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
-    // let url = "mysql://shop-api:shop-api@172.21.192.1:3306/shop-api";
-    let url = "mysql://shop-api:shop-api@db:3306/shop-api";
-    
-    let opt = Opts::from_url(url);
-    let pool = Pool::new(opt.unwrap()).unwrap();
-    let mut _conn = pool.get_conn().unwrap();
-
-    let res = _conn.query_map(
-        "select product_id, product_code, price, name from PRODUCT",
-        |(product_id, product_code, price, name)| 
-          Product {
-                id: product_id,
-                code: product_code,
-                price: price,
-                product_name: name
-            }
-    ).expect("Query failed.");
-     
-    web::Json(res)
-}
-
+use actix_web::{App, HttpServer};
+use controllers::product_controller::{hello, hello2};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -51,7 +10,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(hello2)
     })
-    .bind("0.0.0.0:8080")?
+    .bind("0.0.0.0:12080")?
     .run()
     .await
 }
