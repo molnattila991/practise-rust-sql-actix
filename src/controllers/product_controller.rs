185e92668e2ba61a99d22952999899d888977015
molnarattila991 <molnarattila991@gmail.com>
2022-02-07 19:54:36 +0100

    (edit)

--- src/controllers/product_controller.rs
+++ src/controllers/product_controller.rs
@@ -40,3 +40,28 @@ pub async fn get_list() -> impl Responder {
      
     web::Json(res)
 }
+
+#[get("/list")]
+pub async fn get_list2() -> impl Responder {
+    // let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
+    // let url = "mysql://shop-api:shop-api@172.21.192.1:3306/shop-api";
+    // let url = "mysql://shop-api:shop-api@db:3306/shop-api";
+    let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
+
+    let opt = Opts::from_url(url);
+    let pool = Pool::new(opt.unwrap()).unwrap();
+    let mut _conn = pool.get_conn().unwrap();
+
+    let res = _conn.query_map(
+        "select product_id, product_code, price, name from PRODUCT",
+        |(product_id, product_code, price, name)| 
+          Product {
+                id: product_id,
+                code: product_code,
+                price: price,
+                product_name: name
+            }
+    ).expect("Query failed.");
+     
+    web::Json(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod controllers;
 
 use actix_web::{App, HttpServer};
-use controllers::product_controller::{say_hello, get_list};
+use controllers::product_controller::{say_hello, get_list, get_list2};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -9,6 +9,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(say_hello)
             .service(get_list)
+            .service(get_list2)
     })
     .bind("0.0.0.0:12080")?
     .run()
