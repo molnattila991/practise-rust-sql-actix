ac01835ff5efdb7981d7db5af8c8bf52dd53e15d
molnarattila991 <molnarattila991@gmail.com>
2022-02-08 09:04:56 +0100

    (edit) use appstate

--- src/controllers/product_controller.rs
+++ src/controllers/product_controller.rs
@@ -11,23 +11,20 @@ struct Product {
     product_name: String,
 }
 
+pub struct AppState {
+    pub pool: mysql::Pool,
+}
+
 #[get("/")]
 pub async fn say_hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 #[get("/jsonList")]
-pub async fn get_list() -> impl Responder {
-    // let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
-    // let url = "mysql://shop-api:shop-api@172.21.192.1:3306/shop-api";
-    // let url = "mysql://shop-api:shop-api@db:3306/shop-api";
-    let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
+pub async fn get_list(data: web::Data<AppState>) -> impl Responder {
+    let mut conn = data.pool.get_conn().unwrap();
 
-    let opt = Opts::from_url(url);
-    let pool = Pool::new(opt.unwrap()).unwrap();
-    let mut _conn = pool.get_conn().unwrap();
-
-    let res = _conn.query_map(
+    let res = conn.query_map(
         "select product_id, product_code, price, name from PRODUCT",
         |(product_id, product_code, price, name)| 
           Product {
@@ -42,17 +39,10 @@ pub async fn get_list() -> impl Responder {
 }
 
 #[get("/list")]
-pub async fn get_list2() -> impl Responder {
-    // let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
-    // let url = "mysql://shop-api:shop-api@172.21.192.1:3306/shop-api";
-    // let url = "mysql://shop-api:shop-api@db:3306/shop-api";
-    let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
-
-    let opt = Opts::from_url(url);
-    let pool = Pool::new(opt.unwrap()).unwrap();
-    let mut _conn = pool.get_conn().unwrap();
-
-    let res = _conn.query_map(
+pub async fn get_list3(data: web::Data<AppState>) -> impl Responder {
+    let mut conn = data.pool.get_conn().unwrap();
+    
+    let res = conn.query_map(
         "select product_id, product_code, price, name from PRODUCT",
         |(product_id, product_code, price, name)| 
           Product {
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 mod controllers;
 
-use actix_web::{App, HttpServer};
-use controllers::product_controller::{say_hello, get_list, get_list2};
+use actix_web::{web, App, HttpServer};
+use controllers::product_controller::{get_list, get_list3, say_hello, AppState};
+use mysql::Opts;
+use mysql::Pool;
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let url = "mysql://shop-api:shop-api@db:3306/shop-api";
+    // let url = "mysql://shop-api:shop-api@localhost:3306/shop-api";
+
+    let opt = Opts::from_url(url);
+    // let pool = Pool::new_manual(1,10, opt.unwrap()).unwrap();
+    let pool = Pool::new(opt.unwrap()).unwrap();
+    let mut _conn = pool.get_conn().unwrap();
+
+    let app_data = web::Data::new(AppState { pool: pool });
+    HttpServer::new(move || {
         App::new()
+            .app_data(app_data.clone())
             .service(say_hello)
             .service(get_list)
-            .service(get_list2)
+            .service(get_list3)
     })
     .bind("0.0.0.0:12080")?
     .run()
     .await
-    
 }
